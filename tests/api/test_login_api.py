cfdeb4ebc3de4846f63f48546000af427556a586
PRAMOD PULICHERLA <pramodpulicherla279@gmail.com>
2025-05-15 16:06:42 +0530

    16:02

--- tests/api/test_login_api.py
+++ tests/api/test_login_api.py
@@ -145,7 +145,7 @@ def test_get_organization():
     """Test organization data retrieval"""
     url = f"{BASE_URL}/api/v1/get-organizition"
     params = {"org_id": 48, "ogs_id": 115}
-    auth_token = "yJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5zdGFnaW5nLmtyaXNoaXZhYXMuYWkvYXBpL3YxL2xvZ2luIiwiaWF0IjoxNzQ3MjE2MzQyLCJleHAiOjE3NDk4MDgzNDIsIm5iZiI6MTc0NzIxNjM0MiwianRpIjoiVkJKdGJ4YmdFUVN4bmo2NCIsInN1YiI6IjEzODQiLCJwcnYiOiJmZjY2ZDA3ZjZkNDYyNzMzYmIyOWMzN2QyYTU5YmViZjZiZDY2NjQwIn0.OXzxNUpXpnoxl20s9B_-imreypJ07SZYXkkUD2EuSO0"
+    auth_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5zdGFnaW5nLmtyaXNoaXZhYXMuYWkvYXBpL3YxL2xvZ2luIiwiaWF0IjoxNzQ3MjE2MzQyLCJleHAiOjE3NDk4MDgzNDIsIm5iZiI6MTc0NzIxNjM0MiwianRpIjoiVkJKdGJ4YmdFUVN4bmo2NCIsInN1YiI6IjEzODQiLCJwcnYiOiJmZjY2ZDA3ZjZkNDYyNzMzYmIyOWMzN2QyYTU5YmViZjZiZDY2NjQwIn0.OXzxNUpXpnoxl20s9B_-imreypJ07SZYXkkUD2EuSO0"
     headers = {"Authorization": f"Bearer {auth_token}"}
 
     with allure.step("Making organization data request"):
